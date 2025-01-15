fec83e536bc71fd43cb74b217d6581e948e10d6d
lordypopdy-lab <deelordpopdy2@gmail.com>
2025-01-15 18:22:45 +0100

    safe

--- src/pages/Withdraw.jsx
+++ src/pages/Withdraw.jsx
@@ -9,6 +9,8 @@ const Withdraw = () => {
 
     const [user, setUser] = useState([]);
     const [loading1, setLoading1] = useState(false);
+    const [loading2, setLoading2] = useState(false);
+    const [dataCrypto, setdataCrypto] = useState({ value: "", walletAddress: "" })
     const [data, setData] = useState({ value: 0, bank_name: "", account_name: "", account_number: "", swift_code: "" })
 
     useEffect(() => {
@@ -18,6 +20,15 @@ const Withdraw = () => {
         }
         getUser();
     }, [])
+
+    const handleCopy = async (textToCopy) => {
+        try {
+            await navigator.clipboard.writeText(textToCopy);
+            toast.success("Copied successfully!");
+        } catch (err) {
+            toast.error("Failed to copy!")
+        }
+    };
 
     const bankWithdraw = async () => {
         setLoading1(true);
@@ -52,6 +63,36 @@ const Withdraw = () => {
             console.log("Withrawal failed: ", error);
         }
     }
+    const cyptoWithrawal = async (e) => {
+        e.preventDefault();
+        setLoading2(true);
+        const email = user.email;
+        const { value, walletAddress } = dataCrypto;
+
+        try {
+            await axios.post("/withdrawCrypto", {
+                email,
+                value,
+                walletAddress
+            }).then((data) => {
+                if (data.data.success) {
+                    toast.success(data.data.success);
+                    setLoading2(false);
+                    setdataCrypto({
+                        value: "",
+                        walletAddress: ""
+                    })
+                }else if(data.data.error){
+                    toast.error(data.data.error);
+                    setLoading2(false);
+                }
+            })
+        } catch (error) {
+            console.log("Crypto withdrawal failed: ", error);
+        }
+
+    }
+
     return (
         <>
             <MainNavBar />
@@ -66,6 +107,7 @@ const Withdraw = () => {
                                         <div className="card-body card-gradient">
                                             <h4 className="card-title">Withdraw</h4>
                                             <p className="card-description">Fill | in withdrawal | details</p><br />
+                                            <h5 onClick={() => handleCopy(user._id)}>ID: {user._id && user._id}<i style={{ cursor: "pointer" }} className="fas fa-copy text-warning m-1"></i></h5>
                                             <FadeLoader
                                                 color="#36d7b7"
                                                 loading={loading1}
@@ -74,7 +116,7 @@ const Withdraw = () => {
                                             />
                                             <form action="withdrawal_validation.php" method="post" className="forms-sample">
                                                 <div className="form-group">
-                                                    <label>Amount $currency</label>
+                                                    <label>Amount{user.currency && user.currency}</label>
                                                     <input
                                                         type="number"
                                                         value={data.value}
@@ -129,14 +171,34 @@ const Withdraw = () => {
                                         <div className="card-body card-gradient">
                                             <h4 className="card-title">Withdraw Using Bitcoin | USDT(TRC20)</h4>
                                             <p className="card-description">Bitcoin | USDT Deposit Method</p><br />
-                                            <form action="withdrawal_stats.php" method="post" className="forms-sample">
-
-                                                <h5>UniqueID</h5>
-
+                                            <form onSubmit={cyptoWithrawal} className="forms-sample">
+                                                <h5 onClick={() => handleCopy(user._id)}>ID: {user._id && user._id}<i style={{ cursor: "pointer" }} className="fas fa-copy text-warning m-1"></i></h5>
                                                 <p className="card-description"><span className="text-warning">Please make sure you upload your payment proof for quick payment verification On confirmation,</span> <span className="text-primary">our system will automatically Fund your account and notify you via email address.</span></p>
                                                 <div className="form-group">
-                                                    <label for="exampleInputConfirmPassword1">Amount$</label>
-                                                    <input type="number" className="form-control bg-transparent" placeholder="$" required />
+                                                    <label for="exampleInputConfirmPassword1">Amount{user.currency && user.currency}</label>
+                                                    <input
+                                                        type="number"
+                                                        value={dataCrypto.value}
+                                                        onChange={(e) => setdataCrypto({ ...dataCrypto, value: e.target.value })}
+                                                        className="form-control bg-transparent"
+                                                        placeholder={user.currency && user.currency}
+                                                    />
+                                                </div>
+                                                <FadeLoader
+                                                    color="#36d7b7"
+                                                    loading={loading2}
+                                                    speedMultiplier={3}
+                                                    style={{ textAlign: 'center', position: 'absolute', left: '50%', zIndex: '1', top: "46%" }}
+                                                />
+                                                <div className="form-group mt-3">
+                                                    <label for="exampleInputConfirmPassword1">Recipient Address (ERC20)</label>
+                                                    <input
+                                                        type="text"
+                                                        value={dataCrypto.walletAddress}
+                                                        onChange={(e) => setdataCrypto({ ...dataCrypto, walletAddress: e.target.value })}
+                                                        className="form-control bg-transparent"
+                                                        placeholder="wallet address..."
+                                                    />
                                                 </div>
                                                 <div className="mt-3 d-flex justify-content-between">
                                                     <button type="submit" className="btn btn-primary mr-2">Withdraw<i className="fas fa-arrow-down text-warning m-1"></i></button>
